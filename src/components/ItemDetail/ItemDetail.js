088fc1ec29d7b8ab78c3044417304d4fb2a8dea8
matiasdelgiudice <matidelgiudice@gmail.com>
2022-09-14 17:49:31 -0300

    Vinculo ItemDetail con ItemCount

--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -1,13 +1,20 @@
-
+import { useState } from "react";
+import  ItemCount from "../ItemCount/ItemCount"
 
  const ItemDetail = ({item}) => {
-    const { image, model, brand, year, description, price } = item
+    const { image, model, brand, year, description, price } = item;
+    const [counter, setCounter] = useState(0);
+    const onAdd = (datum)=>{
+        setCounter(datum)
+    }
+
     return (
         <div>
             <img src={image} alt={model} />
             <h1>{brand} {model}</h1>
             <p>{year} ${price}</p>
             <p>{description}</p>
+            <ItemCount stock={10} initial={1} onAdd={onAdd}/>
         </div>
     )
 }
